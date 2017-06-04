05e499b14d8102a7d18eabb3998b8fde2a84e0cc
GEETHANJALI1997 <genegeethanjali@gmail.com>
2017-06-04 11:08:09 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+public class vowel
+{
+public static void main(Strng args[])
+{
+Scanner sc=new Scanner(System.in);
+char ch=sc.next().charAt(0);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+System.out.print("YOU HAVE ENTERED AN VOWEL);
+}
+else
+{
+System.out.print("constant");
+}
+}
+}
